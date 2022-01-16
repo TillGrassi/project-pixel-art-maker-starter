908a43f6f64e1e2ef86efda20dcedd5091889eec
TillGrassi <97838495+TillGrassi@users.noreply.github.com>
2022-01-16 17:16:20 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -4,28 +4,31 @@ const height = document.getElementById('inputHeight');
 const width = document.getElementById('inputWidth');
 const color = document.getElementById('colorPicker');
 const canvas = document.getElementById('pixelCanvas');
-// When size is submitted by the user, call makeGrid()
 
+/*function makeGrid creates a grid with the size of the
+use input and creates the event listener to colorize the cells*/
 function makeGrid(height, width) {
-  for (let z = 0; z < height; z++) {
-    let row = canvas.insertRow(z);
-    for (let y = 0; y < width; y++) {
-      let cell = row.insertCell(y);
-      cell.addEventListener('click', function(event) {
-        cell.style.backgroundColor = color.value;
-      cell.addEventListener('contextmenu', function(event) {
-        event.preventDefault();
-        cell.style.backgroundColor = 'white';
+	for (let z = 0; z < height; z++) {
+		let row = canvas.insertRow(z);
+		for (let y = 0; y < width; y++) {
+			let cell = row.insertCell(y);
+			cell.addEventListener('click', function(event) {
+				cell.style.backgroundColor = color.value;
+				cell.addEventListener('contextmenu', function(event) {
+					event.preventDefault();
+					cell.style.backgroundColor = 'white';
 
-        })
-        })
-    }
-  }
+				})
+			})
+		}
+	}
 }
+/* this function resets the grid by clicking the submit button
+and also calls the makeGrid function */
 sizePicker.addEventListener('submit', function(event) {
-  event.preventDefault();
-  while (canvas.hasChildNodes()) {
-    canvas.removeChild(canvas.firstChild)
-  }
-  makeGrid(height.value, width.value);
+	event.preventDefault();
+	while (canvas.hasChildNodes()) {
+		canvas.removeChild(canvas.firstChild)
+	}
+	makeGrid(height.value, width.value);
 })
